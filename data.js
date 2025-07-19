8349c443c5e4423b72f97e215cea2a7a45aaa383
MatteoIU <matteo.lipsch@iu-study.org>
2025-07-19 13:08:24 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,31 @@
+const emissionsData = [
+  { country: "Deutschland", company: "Beispiel AG", emissions: 180 },
+  { country: "USA", company: "OilPower Inc.", emissions: 320 },
+  { country: "China", company: "Coal Corp", emissions: 410 },
+  { country: "Frankreich", company: "EcoGreen SA", emissions: 95 },
+  { country: "Indien", company: "CarbonMax Ltd.", emissions: 260 },
+
+  { country: "Deutschland", company: "AutoPower GmbH", emissions: 205 },
+  { country: "USA", company: "FutureGas Corp", emissions: 310 },
+  { country: "China", company: "SkyFuel Group", emissions: 395 },
+  { country: "Brasilien", company: "Amazon Energy", emissions: 175 },
+  { country: "Indonesien", company: "TropicOil Co", emissions: 220 },
+
+  { country: "Russland", company: "SiberGas", emissions: 300 },
+  { country: "Kanada", company: "NorthEnergy Ltd.", emissions: 240 },
+  { country: "Südafrika", company: "Cape Carbon", emissions: 210 },
+  { country: "Australien", company: "DownUnder Mining", emissions: 330 },
+  { country: "Deutschland", company: "GreenTech AG", emissions: 120 },
+
+  { country: "Frankreich", company: "Carbon Solutions", emissions: 150 },
+  { country: "China", company: "EastCoal Ltd.", emissions: 420 },
+  { country: "Indien", company: "EcoIndia Pvt.", emissions: 190 },
+  { country: "USA", company: "MegaOil Inc.", emissions: 360 },
+  { country: "Italien", company: "Emissio SpA", emissions: 140 },
+
+  { country: "Spanien", company: "SolarCarbon SL", emissions: 130 },
+  { country: "Japan", company: "Nippon Emission Tech", emissions: 215 },
+  { country: "Südkorea", company: "Korea Fossil Co", emissions: 265 },
+  { country: "Saudi-Arabien", company: "Desert Fuel", emissions: 500 },
+  { country: "Ägypten", company: "Nile Energy", emissions: 185 }
+];
